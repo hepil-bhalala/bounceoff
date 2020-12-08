2b69e8d99522ffffeda2f0a5b3243f8a23e48e9c
hepil-bhalala <73937971+hepil-bhalala@users.noreply.github.com>
2020-12-08 15:51:46 +0530

    Add files via upload

---
+++ mylibray.js
@@ -0,0 +1,16 @@
+function bounceoff(a,b){
+    if (a.x - b.x < b.width/2 + a.width/2
+      && b.x - a.x < b.width/2 + a.width/2) {
+    a.velocityX = movingRect.velocityX * (-1);
+    b.velocityX = b.velocityX * (-1);
+  }
+  if (a.y - b.y < b.height/2 + a.height/2
+    && b.y - a.y < b.height/2 + a.height/2){
+    a.velocityY = a.velocityY * (-1);
+    b.velocityY = b.velocityY * (-1);
+  }
+  
+  
+  
+  
+  }
